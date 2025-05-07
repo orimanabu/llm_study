e4b50f098b5151bc72887090f104dc6e2cc8036d
Manabu Ori <manabu.ori@gmail.com>
2025-05-07 23:50:21 +0900

    update

--- simple/do.sh
+++ simple/do.sh
@@ -6,6 +6,85 @@ if [ x"$#" != x"1" ]; then
 fi
 subcmd=$1; shift
 
+#models="gemma3:1b gemma3:4b qwen2.5:0.5b qwen2.5:1.5b qwen2.5:3b qwen2.5:7b qwen3:0.6b qwen3:1.7b qwen3:4b qwen3:8b deepseek-r1:1.5b deepseek-r1:7b deepseek-r1:8b llama3 llama3.1:8b llama3.2:1b llama3.2:3b mistral:7b granite3-dense:2b granite3-dense:8b granite3.2:2b granite3.2:8b tinyllama smollm2:135m smollm2:360m smollm2:1.7b"
+models_pc="
+gemma3:1b
+gemma3:4b
+qwen2.5:0.5b
+qwen2.5:1.5b
+qwen2.5:3b
+qwen2.5:7b
+qwen3:0.6b
+qwen3:1.7b
+qwen3:4b
+qwen3:8b
+deepseek-r1:1.5b
+deepseek-r1:7b
+deepseek-r1:8b
+llama3
+llama3:8b
+llama3.1:8b
+llama3.2:1b
+llama3.2:3b
+mistral:7b
+granite3-dense:2b
+granite3-dense:8b
+granite3.2:2b
+granite3.2:8b
+tinyllama
+smollm2:135m
+smollm2:360m
+smollm2:1.7b
+"
+models="
+gemma3:1b
+gemma3:4b
+gemma3:12b
+gemma3:27b
+qwen3:0.6b
+qwen3:1.7b
+qwen3:4b
+qwen3:8b
+qwen3:14b
+qwen3:30b
+qwen3:32b
+qwen3:235b
+deepseek-r1:1.5b
+deepseek-r1:7b
+deepseek-r1:8b
+deepseek-r1:14b
+deepseek-r1:32b
+deepseek-r1:70b
+deepseek-r1:671b
+llama3.3:70b
+mistral-small3.1:24b
+llama3.2:1b
+llama3.2:3b
+llama3.1:8b
+llama3.1:70b
+llama3.1:405b
+mistral:7b
+llama3:8b
+llama3:70b
+qwen2.5:0.5b
+qwen2.5:1.5b
+qwen2.5:3b
+qwen2.5:7b
+qwen2.5:14b
+qwen2.5:32b
+qwen2.5:72b
+granite3-dense:2b
+granite3-dense:8b
+granite3.1-dense:2b
+granite3.1-dense:8b
+granite3.2:2b
+granite3.2:8b
+tinyllama
+smollm2:135m
+smollm2:360m
+smollm2:1.7b
+"
+
 case ${subcmd} in
 prepare)
 	python -m venv .venv
@@ -32,7 +111,9 @@ curl)
 	curl -s http://localhost:8080/v1/chat/completions -H "Content-Type: application/json" -H "Authorization: Bearer no-key" -d @chat.json | jq -r '.choices[0].message.content'
 	;;
 pull)
-	for model in gemma3:1b gemma3:4b qwen3:0.6b qwen3:1.7b qwen3:4b deepseek-r1:1.5b llama3 granite3-dense:2b granite3.2:2b; do
+	_models=$(echo ${models} | tr '\n' ' ')
+	_models=${models}
+	for model in ${_models}; do
 		echo "=> pulling ${model}..."
 		ramalama pull ${model}
 	done
@@ -48,6 +129,15 @@ serve-mac-podman)
 	;;
 serve-nvidia)
 	ramalama serve --device nvidia.com/gpu=all --port 8080 --name myllm llama3
+	;;
+bench-all)
+	for model in ${models}; do
+		echo "=> benchmarking for ${model}..."
+		time ramalama --nocontainer bench ${model}
+	done
+	;;
+llama-bench-ngen)
+	time llama-bench -o json -n 64,128,256,512,1024,2048 -r 3 -m $(ramalama inspect --json qwen3:1.7b | jq -r .Path) > log.json
 	;;
 *)
 	echo "unknown subcmd: ${subcmd}"
